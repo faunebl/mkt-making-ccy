1771701227f8f5ea79e9fc3ece9d76a1c2d2ebc5
faunebl <faunebl@protonmail.com>
2025-04-17 17:32:19 +0200

    adding post market order function

--- src/mktmakingccy/trade.py
+++ src/mktmakingccy/trade.py
@@ -1,10 +1,7 @@
 from order_book import OrderBook
 from datetime import datetime
-import polars as pl
 from typing import Literal, List, Dict, Any
 from dataclasses import dataclass, field
-
-#TODO : better history management ?
 
 @dataclass
 class TradeHistory:
@@ -78,64 +75,57 @@ class MarketOrder():
             self.side = side.strip().lower()
         else:
             raise Exception("The side argument should be either 'ask' or 'bid'. \nPlease input a valid argument")
-    def post_market_order(self, orderbook: OrderBook):
+    def post_market_order(self, orderbook: OrderBook, price: float = None):
         if self.side == "bid":
-            # making sure the trade isnt bigger than the whole order book
-            max_size = orderbook.asks.select('size_ask').sum().to_series().to_list()[0] # this is 0 if there are no asks in the order book
-            if self.size > max_size:
-                remaining_size = max_size - self.size
-                print(f"The order size is superior to the depth of the order book. Creating order of size {remaining_size}")
-                # we get the worst possible price
-                worst_price, _, _ = orderbook.asks.bottom_k(k=1, by='ask').row(index=0) #gives us the worst available ask
-                #we delete all asks
-                orderbook.asks.filter(pl.col('ask').is_null()) #deletes all asks
-                # posting new order
-                orderbook.update_order(
-                    price=worst_price,
-                    size=remaining_size,
-                    side='bid'
-                )
-            else:
-                best_price, best_size, _ = orderbook.get_best_ask() # you buy at the ask and sell at the bid
 
-                if self.size <= best_size:
-                        orderbook.update_order(
-                            price = best_price, 
-                            size = best_size - self.size, 
-                            side = 'ask')
-                else:
-                    orderbook.delete_order(price=best_price, size=best_size, side='ask') #delete best order
-                    self.size -= best_size #updating the size
-                    orderbook = self.post_market_order(orderbook) #calling the function recursively
+            best_price, best_size, _ = orderbook.get_best_ask() # you buy at the ask and sell at the bid
+
+            if (best_price is None) and (best_size is None): #there are no asks in the order book
+                print(f"We have reached the end of the order book. Posting order of size {self.size}")
+                orderbook.update_order(
+                    price=price,
+                    size=self.size,
+                    side='bid'
+                ) # we post a new bid 
+                #no trade to log here until that order is lifted 
+
+            if self.size <= best_size:
+                    orderbook.update_order(
+                        price = best_price, 
+                        size = best_size - self.size, 
+                        side = 'ask')
+                    #logging the trade
+                    history.log(side='buy', price=best_price, size=best_size)
+            elif self.size > best_size:
+                orderbook.delete_order(price=best_price, size=best_size, side='ask') #delete best order
+                history.log(side='buy', price=best_price, size=best_size) #logging the trade
+                self.size -= best_size #updating the size
+                orderbook = self.post_market_order(orderbook, best_price) #calling the function recursively
 
         elif self.side == "ask": # same thing but with bid
-            # making sure the trade isnt bigger than the whole order book
-            max_size = orderbook.asks.select('size_bid').sum().to_series().to_list()[0] # this is 0 if there are no asks in the order book
-            if self.size > max_size:
-                remaining_size = max_size - self.size
-                print(f"The order size is superior to the depth of the order book. Creating order of size {remaining_size}")
-                # we get the worst possible price
-                worst_price, _, _ = orderbook.bids.bottom_k(k=1, by='bid').row(index=0) #gives us the worst available ask
-                #we delete all asks
-                orderbook.asks.filter(pl.col('bid').is_null()) #deletes all asks
-                # posting new order
-                orderbook.update_order(
-                    price=worst_price,
-                    size=remaining_size,
-                    side='ask'
-                )
-            else:
-                best_price, best_size, _ = orderbook.get_best_bid() # you buy at the ask and sell at the bid
+            best_price, best_size, _ = orderbook.get_best_bid() # you buy at the ask and sell at the bid
 
-                if self.size <= best_size:
-                        orderbook.update_order(
-                            price = best_price, 
-                            size = best_size - self.size, 
-                            side = 'bid')
-                else:
-                    orderbook.delete_order(price=best_price, size=best_size, side='ask') #delete best order
-                    self.size -= best_size #updating the size
-                    orderbook = self.post_market_order(orderbook) #calling the function recursively
+            if (best_price is None) and (best_size is None):
+                print(f"We have reached the end of the order book. Posting order of size {self.size}")
+                orderbook.update_order(
+                    price=price,
+                    size=self.size,
+                    side='ask'
+                ) # we post a new ask 
+                #no trade to log here until that order is lifted 
+
+            if self.size <= best_size:
+                    orderbook.update_order(
+                        price = best_price, 
+                        size = best_size - self.size, 
+                        side = 'bid')
+                    #logging the trade
+                    history.log(side='sell', price=best_price, size=best_size)
+            elif self.size > best_size:
+                orderbook.delete_order(price=best_price, size=best_size, side='bid') #delete best order
+                history.log(side='sell', price=best_price, size=best_size) #logging the trade
+                self.size -= best_size #updating the size
+                orderbook = self.post_market_order(orderbook, best_price) #calling the function recursively
 
 
         return orderbook
